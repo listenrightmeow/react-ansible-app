d33f0ec6928e3ec32f929dbc305f8eaeaa2198be
listenrightmeow <mike@fatt.ist>
2017-12-05 13:58:28 -0800

    stable react starting-point

---
+++ helpers/webpack/aliases/index.js
@@ -0,0 +1,27 @@
+const cwd = process.cwd();
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (root = `${cwd}`) => {
+  const projectRoot = root;
+  const projectScriptDirs = {};
+  const getDirectories = (srcpath) => {
+    return fs.readdirSync(srcpath).filter((file) => {
+      return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
+  };
+
+  ['application'].map((type) => {
+    getDirectories(path.join(projectRoot, type)).map((directory) => {
+      const key = type === 'stylesheets' ? `stylesheets/${directory}` : directory;
+      projectScriptDirs[key] = path.join(projectRoot, type, directory);
+      return null;
+    });
+  });
+
+  return {
+    resolveAlias: Object.assign({
+      src: projectRoot
+    }, projectScriptDirs)
+  };
+};
