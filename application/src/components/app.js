cda5dcedc39d1c804d4d63770e4d2ea1bb036554
listenrightmeow <mike@fatt.ist>
2017-12-05 19:57:57 -0800

    logo animation replacement, structure cleanup from downstream merge

--- application/src/components/app.js
+++ application/src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
 import logo from 'images/logo.svg';
+import 'stylesheets/app.css';
 
 class App extends Component {
   render() {
     return (
       <section data-react-wrapper="app">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="app-logo" alt="logo" />
       </section>
     );
   }
